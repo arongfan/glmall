54cce40024f0b2d8371112fe45ec30480889cbdc
rgf00433054 <Rgf00433054>
2021-04-22 14:53:42 +0800

    新建项目

---
+++ glmall-product/src/test/java/com/smd/glmall/glmallproduct/GlmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smd.glmall.glmallproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/main/java/com/smd/glmall/glmallproduct/GlmallProductApplication.java
@@ -0,0 +1,13 @@
+package com.smd.glmall.glmallproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlmallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlmallProductApplication.class, args);
+    }
+
+}
